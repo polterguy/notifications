55c9e12400e711205b7cf0e30899fc0aed686d53
Aista, Ltd <th@aista.com>
2022-07-02 09:59:31 +0300

    Update notifications.mysql.sql

--- magic.startup/notifications.mysql.sql
+++ magic.startup/notifications.mysql.sql
@@ -16,7 +16,7 @@ use notifications;
  * Notifications primary table.
  */
 create table notifications (
-  id int(11) not null auto_increment,
+  id int(11) primary key not null auto_increment,
   username varchar(256) not null,
   type varchar(50) not null,
   content text not null,
